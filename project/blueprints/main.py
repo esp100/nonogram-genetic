ff06e4b017abe1af3ee326b45c3f100d5fb105aa
Adrian <hastur100hp@gmail.com>
2026-07-29 17:36:53 -0600

    feat: created blueprints/main

---
+++ project/blueprints/main.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, jsonify, request
+from project.domain.nonogram_service import NonogramService
+
+main = Blueprint('main', __name__)
+
+@main.route('/nonogram', methods=['POST'])
+def create_nonogram(nonogram_service: NonogramService):
+    data = request.get_json()
+    rows = data.get('rows')
+    cols = data.get('cols')
+    grid = data.get('grid', None)
+
+    row_hints, col_hints = None, None 
+    #TODO: Implement logic to calculate hints based on the provided grid 
+
+    nonogram = nonogram_service.create_nonogram(rows, cols, row_hints, col_hints, grid)
+    return jsonify(nonogram.get_nonogram()), 201
+
+@main.route('/nonogram/solve', methods=['POST'])
+def solve_nonogram(nonogram_service: NonogramService):
+    data = request.get_json()
+    rows = data.get('rows')
+    cols = data.get('cols')
+    row_hints = data.get('row_hints', [])
+    col_hints = data.get('col_hints', [])
+    grid = data.get('grid', None)
+
+    nonogram = nonogram_service.create_nonogram(rows, cols, row_hints, col_hints, grid)
+    solved_nonogram = nonogram_service.solve_nonogram(nonogram)
+    return jsonify(solved_nonogram.get_nonogram()), 200
+
+@main.route("/nonogram/get", methods=["POST"])
+def get_nonogram(nonogram_service: NonogramService):
+    nonogram = nonogram_service.get_nonogram(nonogram_service)
+    return jsonify(nonogram), 200
+
+
