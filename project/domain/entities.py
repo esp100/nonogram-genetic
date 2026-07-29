21a38717c81c4ce54719612e89819a6df47c8f7b
Adrian <hastur100hp@gmail.com>
2026-07-29 16:36:54 -0600

    feat: added entities.py, the nonogram class

---
+++ project/domain/entities.py
@@ -0,0 +1,31 @@
+class nonogram:
+    def __init__(self, rows, cols, row_hints=[], col_hints=[]):
+        self.rows = rows
+        self.cols = cols
+        self.row_hints = row_hints
+        self.col_hints = col_hints
+        self.grid = [[0 for _ in range(cols)] for _ in range(rows)]
+
+    def set_grid(self, grid):
+        self.grid = grid
+
+    def set_hints(self, row_hints, col_hints):
+        self.row_hints = row_hints
+        self.col_hints = col_hints
+
+    def get_solver(self):
+        return {
+            "rows": self.rows,
+            "cols": self.cols,
+            "row_hints": self.row_hints,
+            "col_hints": self.col_hints
+        }
+
+    def get_nonogram(self):
+        return {
+            "rows": self.rows,
+            "cols": self.cols,
+            "row_hints": self.row_hints,
+            "col_hints": self.col_hints,
+            "grid": self.grid
+        }
