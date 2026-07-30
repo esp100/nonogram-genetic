015d7c4a16887236a2ea173552e9decc78771fe0
Adrian <hastur100hp@gmail.com>
2026-07-30 12:37:46 -0600

    refactor: changed init state to implement csv adapter

--- project/adapters/assembly.py
+++ project/adapters/assembly.py
@@ -1,6 +1,14 @@
 from dependency_injector import containers, providers
 from project.domain.nonogram_service import NonogramServiceImpl
+from project.adapters.csv_adapter import CSVAdapter
 
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(packages=["project.blueprints"])
-    nonogram_service = providers.Factory(NonogramServiceImpl)
+    csv_adapter = providers.Factory(
+        CSVAdapter,
+    )
+
+    nonogram_service = providers.Factory(
+        NonogramServiceImpl,
+        csv_adapter=csv_adapter,
+    )
--- project/domain/nonogram_service.py
+++ project/domain/nonogram_service.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from .entities import Nonogram
+from project.domain.csv_port import CSVPort
 #TODO: Solving algorithm for Nonogram
 
 class NonogramService(ABC):
@@ -15,14 +16,30 @@ class NonogramService(ABC):
     def get_nonogram(self, nonogram: Nonogram):
         pass
 
+    @abstractmethod
+    def generate_file(self, grid):
+        pass
+
 
 class NonogramServiceImpl(NonogramService):
+
+    def __init__(self, csv_adapter: CSVPort = None):
+        self.csv_adapter = csv_adapter
+
     def create_nonogram(self, rows, cols, row_hints=[], col_hints=[], grid=None):
-        return Nonogram(rows, cols, row_hints, col_hints, grid if grid is not None else [[0 for _ in range(cols)] for _ in range(rows)])
+        if grid is None:
+            grid = [[0 for _ in range(cols)] for _ in range(rows)]
+        return Nonogram(grid=grid, row_hints=row_hints, col_hints=col_hints)
+
 
     def solve_nonogram(self, nonogram: Nonogram):
         # TODO Solving 
+        #Also return time, generations, etc. 
         return nonogram
 
     def get_nonogram(self, nonogram: Nonogram):
         return nonogram.get_nonogram()
+
+    def generate_file(self, grid: list[list[int]]):
+        nonogram = Nonogram(grid=grid)
+        return self.csv_adapter.write_stream(nonogram.grid)
