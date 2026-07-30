1cbdd965e9c7bc0fedf827dbd452987bc4cc120c
Adrian <hastur100hp@gmail.com>
2026-07-29 18:00:28 -0600

    feat: added create app

---
+++ project/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from project.adapters.assembly import Container
+import os
+import sys
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+
+    container = Container(app=app)
+    app.container = container
+    container.wire(modules=[sys.modules[__name__]])
+
+    from project.blueprints.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
