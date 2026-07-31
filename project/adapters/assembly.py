31ed2fd499ecd48979eff26ae4e38ad13cdaf9e4
Adrian <hastur100hp@gmail.com>
2026-07-31 16:54:46 -0600

    feat: wired the solver adapter to Container

--- project/adapters/assembly.py
+++ project/adapters/assembly.py
@@ -1,14 +1,19 @@
 from dependency_injector import containers, providers
 from project.domain.nonogram_service import NonogramServiceImpl
 from project.adapters.csv_adapter import CSVAdapter
+from project.adapters.genetic_algorithm_solver import GeneticAlgorithmSolver
 
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(packages=["project.blueprints"])
     csv_adapter = providers.Factory(
         CSVAdapter,
     )
+    nonogram_solver_adapter = providers.Factory(
+        GeneticAlgorithmSolver
+    )
 
     nonogram_service = providers.Factory(
         NonogramServiceImpl,
         csv_adapter=csv_adapter,
+        nonogram_solver_adapter = nonogram_solver_adapter
     )
